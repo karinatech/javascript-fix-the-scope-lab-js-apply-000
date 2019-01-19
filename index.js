a8e8b0033c673e139161dfde4bbb85e6522e4b79
Karyna Dominguez <karinaminty@gmail.com>
2019-01-19 19:10:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ function yourAnimal(){
 
   return animal
 }
-
+ var two = 2
 function add2(n) {
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+ 
 }
 
 var funkyFunction = function() {
